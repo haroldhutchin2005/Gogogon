97c7d2950b53cf5b184b2be0de9cf2f96f303393
Harold Hutchins <160975590+haroldhutchin2005@users.noreply.github.com>
2024-06-10 14:51:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ async function getUploadUrl(instance) {
 }
 
 async function downloadFile(url, outputPath) {
-    const response = await axios.get(`https://ytdlbyjonell-0c2a4d00cfcc.herokuapp.com/yt?url=${encodeURIComponent(url)}&type=mp4`, {
+    const response = await axios.get(`https://ytapibyjonell.adaptable.app/yt?url=${encodeURIComponent(url)}&type=mp4`, {
         responseType: 'arraybuffer',
     });
 
